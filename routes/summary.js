aedec8a1a1eab3fe628b78fb3174345c78998345
Tanay Das <111950834+tanaydas2003@users.noreply.github.com>
2024-10-28 12:51:29 +0530

    Update summary.js

--- routes/summary.js
+++ routes/summary.js
@@ -3,8 +3,6 @@ const { getSummary } = require('../controllers/summaryController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
-
-// GET /summary - Get transaction summary metrics
 router.get('/', authMiddleware, getSummary);
 
 module.exports = router;
