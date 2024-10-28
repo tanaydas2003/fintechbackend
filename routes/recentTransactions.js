b72abc1b3af0c1c5938d79dbb7fbedfc7aa5053b
Tanay Das <111950834+tanaydas2003@users.noreply.github.com>
2024-10-28 12:51:17 +0530

    Update recentTransactions.js

--- routes/recentTransactions.js
+++ routes/recentTransactions.js
@@ -3,8 +3,6 @@ const { getRecentTransactions } = require('../controllers/transactionController'
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
-
-// GET /recent-transactions - Get the latest transactions
 router.get('/', authMiddleware, getRecentTransactions);
 
 module.exports = router;
