d2db1f10dd11e1aee832b1b3bca85ce59b400bb4
Tanay Das <111950834+tanaydas2003@users.noreply.github.com>
2024-10-28 12:50:27 +0530

    Update rateLimiter.js

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  windowMs: 15 * 60 * 1000,
+  max: 100,
   message: 'Too many requests, please try again later.',
 });
 
