fff16ed6800d6e6cce83998fc7df30bae5400665
Tanay Das <111950834+tanaydas2003@users.noreply.github.com>
2024-10-28 12:52:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,23 +11,18 @@ const authRoutes = require('./routes/auth');
 const app = express();
 const PORT = 5000;
 
-// Connect to database
 connectDB();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
 
-// Routes
 app.use('/api/transactions', transactionsRoute);
 app.use('/api/summary', summaryRoute);
 app.use('/api/recent-transactions', recentTransactionsRoute);
 app.use('/api', authRoutes);
 
 
-
-// Error logging middleware
 app.use(errorLogger);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
